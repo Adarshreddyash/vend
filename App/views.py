868e441a2b6c548ce13807435c7bd56838ac610a
Adarshreddy adelli <adarshreddy9849@gmail.com>
2019-03-10 13:22:18 +0530

    Update views.py

--- App/views.py
+++ App/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from App import forms
-
+#fuck
 @login_required(login_url="login/")
 
 def home(request):
